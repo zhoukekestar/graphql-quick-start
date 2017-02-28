ddbe22c02a115ca22950e4e6b0afd1d9d9d4b42d
keke <zhoukekestar@163.com>
2017-02-28 15:26:25 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # graphql-quick-start
 Quick start for GraphQL
+
+# Quick start
+* `npm install babel-cli -g` to install babel.
+* `npm install` to install dependencies.
+* `babel-node index.js` to run script.
---
+++ index.js
@@ -0,0 +1,38 @@
+import {
+	graphql,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLInterfaceType
+} from 'graphql';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        },
+      },
+    },
+  }),
+});
+
+const query = '{ hello }';
+
+graphql(schema, query).then(result => {
+
+  // Prints
+  // {
+  //   data: { hello: "world" }
+  // }
+  console.log(result);
+
+});
